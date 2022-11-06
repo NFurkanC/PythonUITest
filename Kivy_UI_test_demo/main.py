330178649d5a00d72134e668b221f575a0a6917e
Kaj4r <nfurkan5a@gmail.com>
2022-11-06 22:33:33 +0300

    modified:   Kivy_UI_test_demo/aniui.kv 	modified:   Kivy_UI_test_demo/main.py

--- Kivy_UI_test_demo/main.py
+++ Kivy_UI_test_demo/main.py
@@ -4,6 +4,8 @@ from kivy.animation import Animation
 from kivy.uix.image import Image
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.graphics.vertex_instructions import Line
+from kivy.graphics.context_instructions import Color
+from kivy.metrics import dp
 
 class AniBox(Widget):
     pass
@@ -12,7 +14,18 @@ class UIBox(Widget):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         with self.canvas:
-            Line(points=(200,300,600,300),width=3)
+            self.gauge_line = Line(points=(210,300,600,300),width=3)
+            Color(0,1,0)
+    def on_button1_click(self):
+        x, y, x2, y2 = self.gauge_line.points
+        y= y- dp(20)
+        y2 += dp(20)
+        self.gauge_line.points = (x,y,x2,y2)
+    def on_button2_click(self):
+        x, y, x2, y2 = self.gauge_line.points
+        y2= y2- dp(20)
+        y += dp(20)
+        self.gauge_line.points = (x,y,x2,y2)
 #class AniBox aktif değil!
 class AniUI(App):
     pass
