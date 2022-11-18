dbfa009f67bf149de3fe8c621fc74c22d7e282c2
NFurkan <72273576+NFurkanC@users.noreply.github.com>
2022-11-18 13:59:14 +0300

    Details of testser.py
    Python - Arduino Serial Test File

--- testser.py
+++ testser.py
@@ -1,7 +1,7 @@
 import time
 import serial
 
-arduino = serial.Serial('COM4', 9600, timeout=.1)
+arduino = serial.Serial('COM4', 9600, timeout=.1) #Change COM4 with your Arduino port
 while True:
 	data = arduino.readline().rstrip().decode() #the last bit gets rid of the new-line chars
 	print (data)
